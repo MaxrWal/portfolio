d9b0430fa72505321d7991cb9ee5fbb14643e145
Max <maxrwal@gmail.com>
2024-09-04 15:29:01 +0100

    restored page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+  // return (
+  //   <div>
+  //     <h1>Welcome to My Portfolio</h1>
+  //     <p>This is the home page of my portfolio website.</p>
+  //   </div>
+  // )
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
